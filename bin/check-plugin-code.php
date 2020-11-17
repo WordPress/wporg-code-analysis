f142429aed3ec8f6dc1956f1039d22d2f8fef191
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-11-16 17:27:20 -0800

    Update bin script

--- bin/check-plugin-code.php
+++ bin/check-plugin-code.php
@@ -1,9 +1,12 @@
 #!/usr/bin/php
 <?php
+/**
+ * A quick and dirty script for testing the PHPCS class against plugins installed on wporg.
+ */
 
-namespace WordPressdotorg\Plugin_Directory;
+namespace WordPressDotOrg\Code_Analysis;
 
-use WordPressdotorg\Plugin_Directory\Tools\PHPCS;
+use WordPressDotOrg\Code_Analysis\PHPCS;
 
 // This script should only be called in a CLI environment.
 if ( 'cli' != php_sapi_name() ) {
@@ -22,33 +25,34 @@ if ( empty( $opts['report'] ) ) {
 $url = 'https://wordpress.org/plugins/';
 $_SERVER['HTTP_HOST']   = parse_url( $url, PHP_URL_HOST );
 $_SERVER['REQUEST_URI'] = parse_url( $url, PHP_URL_PATH );
-
 $abspath = substr( __DIR__, 0, strpos( __DIR__, 'wp-content' ) );
 require rtrim( $abspath, '/' ) . '/wp-load.php';
 
-if ( ! class_exists( '\WordPressdotorg\Plugin_Directory\Plugin_Directory' ) ) {
-	fwrite( STDERR, "Error! This site doesn't have the Plugin Directory plugin enabled.\n" );
-	if ( defined( 'WPORG_PLUGIN_DIRECTORY_BLOGID' ) ) {
-		fwrite( STDERR, "Run the following command instead:\n" );
-		fwrite( STDERR, "\tphp " . implode( ' ', $argv ) . ' --url ' . get_site_url( WPORG_PLUGIN_DIRECTORY_BLOGID, '/' ) . "\n" );
-	}
-	die();
-}
+// Load phpcs class
+require dirname( __DIR__ ) . '/includes/class-phpcs.php';
 
 $phpcs = new PHPCS();
 $phpcs->set_standard( dirname( __DIR__ ) . '/rulesets/reviewer-flags.xml' );
 
 $path = dirname( dirname( __DIR__ ) ) . '/' . $opts['slug'];
 $args = array(
-	'extensions' => 'php',
-	's' => true,
+	'extensions' => 'php', // Only check php files.
+	's' => true, // Show the name of the sniff triggering a violation.
 );
 
-echo $path;
+echo "Checking $path...\n";
 
 switch ( $opts['report'] ) {
 	case 'full':
 		echo $phpcs->run_full_report( $path, $args );
+		break;
+	case 'json':
+		$result = $phpcs->run_json_report( $path, $args, 'array' );
+		if ( is_array( $result ) ) {
+			print_r( $result );
+		} else {
+			echo $result;
+		}
 		break;
 	case 'summary':
 	default:
