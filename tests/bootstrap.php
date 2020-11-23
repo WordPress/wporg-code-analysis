6a72779d5f1630a3b5f442d2dbc6e9a8963367da
tellyworth <alex@automattic.com>
2020-11-23 15:34:44 +1100

    Add some stubs so the class works during testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,29 @@
 <?php
 
+// Fake WP_Error class so the PHPCS class works
+class WP_Error {
+	var $code;
+	var $message;
+	var $data;
+
+	function __construct( $code = '', $message = '', $data = '' ) {
+		$this->code = $code;
+		$this->message = $message;
+		$this->data = $data;
+	}
+
+	public function __toString() {
+		return var_export( $this, true );
+	}
+}
+
+// Ditto
+function get_temp_dir() {
+	return '/tmp';
+}
+
+// Again so PHPCS class works
+define( 'WPINC', 'yeahnah' );
+
+
 require_once( dirname( __DIR__ ) . '/includes/class-phpcs.php' );
