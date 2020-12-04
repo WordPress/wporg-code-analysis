818c00e770d885227a912a0093c975e4b8668507
tellyworth <alex@automattic.com>
2020-12-04 17:52:40 +1100

    Add phpunit and starter tests

--- tests/db/DirectDBUnitTest.php
+++ tests/db/DirectDBUnitTest.php
@@ -5,8 +5,8 @@ use PHP_CodeSniffer\Ruleset;
 use PHP_CodeSniffer\Config;
  
 class DisallowExtractSniffTest extends TestCase {
-    public function testDisallowExtractSniff() {
-        $fixtureFile = __FILE__ . '.inc';
+    public function test_unsafe_code() {
+        $fixtureFile = __FILE__ . '-bad.inc';
         $sniffFiles = [ dirname( dirname( __DIR__ ) ) . '/MinimalPluginStandard/Sniffs/DirectDBSniff.php' ];
         $config = new Config();
         $ruleset = new Ruleset($config);
@@ -15,6 +15,24 @@ class DisallowExtractSniffTest extends TestCase {
         $phpcsFile = new LocalFile($fixtureFile, $ruleset, $config);
         $phpcsFile->process();
         $foundErrors = $phpcsFile->getErrors();
+        var_dump( __FUNCTION__, $foundErrors );
+        $lines = array_keys($foundErrors);
+
+        // FIXME
+        $this->assertEquals([7], $lines);
+    }
+
+    public function test_safe_code() {
+        $fixtureFile = __FILE__ . '-safe.inc';
+        $sniffFiles = [ dirname( dirname( __DIR__ ) ) . '/MinimalPluginStandard/Sniffs/DirectDBSniff.php' ];
+        $config = new Config();
+        $ruleset = new Ruleset($config);
+        $ruleset->registerSniffs($sniffFiles, [], []);
+        $ruleset->populateTokenListeners();
+        $phpcsFile = new LocalFile($fixtureFile, $ruleset, $config);
+        $phpcsFile->process();
+        $foundErrors = $phpcsFile->getErrors();
+        var_dump( __FUNCTION__, $foundErrors );
         $lines = array_keys($foundErrors);
 
         // FIXME
