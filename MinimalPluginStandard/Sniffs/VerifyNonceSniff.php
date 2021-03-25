230a57d8dda68afc7c45c1c0195bb6dbde197552
tellyworth <alex@automattic.com>
2021-03-25 14:49:34 +1100

    Fix for new cases

--- MinimalPluginStandard/Sniffs/VerifyNonceSniff.php
+++ MinimalPluginStandard/Sniffs/VerifyNonceSniff.php
@@ -222,8 +222,19 @@ class VerifyNonceSniff extends Sniff {
 
 					// if ( $something && ! wp_verify_nonce( ... ) )
 					if ( $this->expression_contains_and( $expression_start, $expression_end ) && $this->scope_contains_error_terminator( $scope_start, $scope_end ) ) {
-						$expression_functions = array_count_values( $this->find_functions_in_expression( $expression_start, $expression_end ) );
-						if ( isset( $expression_functions[ 'wp_verify_nonce' ] ) && $expression_functions[ 'wp_verify_nonce' ] > 1 ) {
+						$andPtr = $this->expression_contains_and( $expression_start, $expression_end );
+						if ( $andPtr < $stackPtr ) {
+							// if ( ..something.. && ! wp_verify_nonce() ... )
+							$operand_functions = array_count_values( $this->find_functions_in_expression( $expression_start, $andPtr ) );
+							// if ( ... wp_verify_nonce() && ! wp_verify_nonce() ... )
+							if ( isset( $operand_functions[ 'wp_verify_nonce' ] ) ) {
+								// This is ok, and we will have already checked the previous wp_verify_nonce(), so skip.
+								return;
+							}
+
+						} else {
+							// if ( ... !wp_verify_nonce() && ..something.. )
+							// This is ok since the nonce call comes before the &&
 							return;
 						}
 						$this->phpcsFile->addError( 'Unsafe use of wp_verify_nonce() in expression %s.',
@@ -245,9 +256,22 @@ class VerifyNonceSniff extends Sniff {
 
 						// if ( $something || wp_verify_nonce( ... ) )
 						if ( $this->expression_contains_or( $expression_start, $expression_end ) && $this->scope_contains_error_terminator( $scope_start, $scope_end ) ) {
-							$expression_functions = array_count_values( $this->find_functions_in_expression( $expression_start, $expression_end ) );
-							if ( isset( $expression_functions[ 'wp_verify_nonce' ] ) && $expression_functions[ 'wp_verify_nonce' ] > 1 ) {
-								return;
+
+							$orPtr = $this->expression_contains_or( $expression_start, $expression_end );
+							if ( $orPtr < $stackPtr ) {
+								// if ( ..something.. || wp_verify_nonce() ... )
+								$operand_functions = array_count_values( $this->find_functions_in_expression( $expression_start, $orPtr ) );
+								// If the previous "something" was another wp_verify_nonce() call then we're fine, ignore
+								if ( isset( $operand_functions[ 'wp_verify_nonce' ] ) ) {
+									return;
+								}
+							} else {
+								// if ( wp_verify_nonce || ..something.. )
+								$operand_functions = array_count_values( $this->find_functions_in_expression( $orPtr, $expression_end ) );
+								// If the next "something" is another wp_verify_nonce() call then we're fine, ignore
+								if ( isset( $operand_functions[ 'wp_verify_nonce' ] ) ) {
+									return;
+								}
 							}
 							$this->phpcsFile->addError( 'Possibly unsafe use of wp_verify_nonce() in expression %s.',
 								$stackPtr,
