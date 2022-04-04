62f5ad6a0cbf80d06ee3fee762888d6a20faf84a
tellyworth <alex@automattic.com>
2022-04-04 10:28:32 +1000

    Use line marks in trac browser view

--- admin/class-scan-metabox.php
+++ admin/class-scan-metabox.php
@@ -45,7 +45,12 @@ class Scan_Metabox {
 				if ( 'WARNING' === $message[ 'type' ] ) {
 					continue;
 				}
-				printf( "%s %s in <a href='https://plugins.trac.wordpress.org/browser/%s/%s/%s#L%d'>%s line %d</a>\n", esc_html( $message[ 'type' ] ), esc_html( $message[ 'source' ] ), esc_attr( $slug ), esc_attr( $tag_dir ), esc_attr( $filename ), $message[ 'line' ], esc_html( $filename ), $message[ 'line' ] );
+
+				$marks = [];
+				if ( preg_match_all( '/ at line (\d+):/', $message[ 'message' ], $matches, PREG_PATTERN_ORDER ) ) {
+					$marks[] = array_map( 'intval', $matches[1] );
+				}
+				printf( "%s %s in <a href='https://plugins.trac.wordpress.org/browser/%s/%s/%s%s#L%d'>%s line %d</a>\n", esc_html( $message[ 'type' ] ), esc_html( $message[ 'source' ] ), esc_attr( $slug ), esc_attr( $tag_dir ), esc_attr( $filename ), $message[ 'line' ], esc_html( $filename ), ($marks ? '?marks=' . join( ',', $marks ) : '' ), $message[ 'line' ] );
 				echo esc_html( $message[ 'message' ] ) . "\n";
 				if ( $message['context'] ) {
 					foreach ( $message['context'] as $line_no => $context_line ) {
