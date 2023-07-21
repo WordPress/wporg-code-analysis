ed2f4d4dbfc903a8ee54b41093ed7838b75ba14a
tellyworth <alex@automattic.com>
2023-07-21 15:10:49 +1000

    Put messages inline instead of overlay

--- admin/class-scan-detail.php
+++ admin/class-scan-detail.php
@@ -64,21 +64,39 @@ class Scan_Detail {
 		// TODO: cache this?
 		$results = Scanner::get_scan_results_for_plugin( $post, $version, $file );
 
+		printf(
+			'<h1>Scan Detail for %s</h1>',
+			esc_html( $post->post_title )
+		);
+		printf(
+			'<p>Version: %s<br/>File: %s</p>',
+			esc_html( $version ),
+			esc_html( $file )
+		);
+		printf(
+			'<p>Found %d errors and %d warnings in %d (%0.2fs).</p>',
+			$results[ 'totals' ][ 'errors' ],
+			$results[ 'totals' ][ 'warnings' ],
+			$file,
+			$results[ 'time_taken' ]
+		);
+
 		#echo '<pre style="white-space: pre-wrap;">';
 		#var_dump( $version, $file, $results );
 		#echo '</pre>';
 
 		if ( isset( $results[ 'realfile' ] ) ) {
-			$first_line = 0;
+			$first_line = 1;
 			$line_number = 0;
 			$code_class = 'language-php';
 
 			$messages_by_line = array();
 
 			foreach ( $results[ 'files' ][ $file ][ 'messages' ] as $message ) {
-				$messages_by_line[ $message[ 'line' ] ][] = $message;
+				$messages_by_line[ intval($message[ 'line' ]) ][] = $message;
 			}
 
+			#echo '<pre>'; var_dump( $messages_by_line ); echo '</pre>';
 			$highlight_lines = array_map( 'intval', array_keys( $messages_by_line ) );
 
 			echo '<pre class="line-numbers" data-start="' . intval($first_line) . '" data-line-offset="' . intval($first_line) . '" data-line="' . join(',', $highlight_lines) . '"><code language="php" class="' . $code_class . '">';
@@ -88,16 +106,28 @@ class Scan_Detail {
 				++ $line_number;
 
 				if ( isset( $messages_by_line[$line_number] ) ) {
-					$type = $messages_by_line[$line_number][0]['type'];
-					echo '<mark id="line-' . intval( $line_number ) . '" class="' . esc_attr($type) . '"><b>' . rtrim( esc_html( $line ) ) . '</b></mark>';
-				} else {
-					echo rtrim( esc_html( $line ) );
-				}
+					$type = strtolower($messages_by_line[$line_number][0]['type']);
+					echo '<mark id="line-' . intval( $line_number ) . '" class="message-' . esc_attr($type) . '"><b>' . esc_html( rtrim( $line ) ) . '</b></mark>' . "\n";
+					echo '</code></pre>';
+					foreach ( $messages_by_line[$line_number] as $msg ) {
+						echo '<div class="message-' . esc_attr( strtolower( $msg[ 'type' ] ) ) . '">';
+						echo '<p>' . esc_html( $msg[ 'type' ] ) . esc_html( $msg[ 'source' ] ) . ' on line ' . esc_html( $line_number ) . '</p>';
+						echo '<p>' . esc_html( $msg[ 'message' ] ) . '</p>';
+						#var_dump( $msg );
+						echo '</div>';
+					}
+					$highlight_lines = array_filter( array_map( 'intval', array_keys( $messages_by_line ) ),
+						fn( $line ) => $line > $line_number );
 
-				echo "\n";
+					echo '<pre class="line-numbers" data-start="' . intval($line_number + 1) . '" data-line-offset="' . intval($line_number) . '" data-line="' . join(',', $highlight_lines) . '"><code language="php" class="' . $code_class . '">';
+
+				} else {
+					echo esc_html( rtrim($line) ) . "\n";
+				}
 			}
 			echo '</code></pre>';
 
+			/*
 			foreach ( $messages_by_line as $line => $message ) {
 				echo '<div class="message-detail" id="message-' . intval( $line ) . '" data-line="' . intval( $line ) . '">';
 				foreach ( $message as $msg ) {
@@ -106,7 +136,7 @@ class Scan_Detail {
 					echo '</div>';
 				}
 				echo '</div>';
-			}
+			}*/
 		}
 
 		if ( ! $results || empty( $results[ 'files' ] ) ) {
